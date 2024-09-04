3ca247be1e86f03371f70f66d1d83062d7184a5b
vignu <vignuday2501@gmail.com>
2024-09-04 15:56:27 +0530

    added speed insights and analytics

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
 import DashBoard from "./components/DashBoard";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 export default function Home() {
   return (
     <>
       <DashBoard></DashBoard>
+      <SpeedInsights/>
+      <Analytics/>
     </>
   );
 }
