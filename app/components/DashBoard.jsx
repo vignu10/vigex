d30324da1772799d8a7e2df3f4ad00ad6377d86e
vigneshwar <vcu@metropolis.io>
2024-10-26 18:08:09 +0530

    newaxio  refereence

--- app/components/DashBoard.jsx
+++ app/components/DashBoard.jsx
@@ -4,9 +4,10 @@ import TransactionDataTable from "@/app/components/TransactionDataTable";
 export default function DashBoard() {
   return (
     <>
-    <main className="p-3 m-3 mr-5">
-      <NavBar title="here" />
-      <TransactionDataTable></TransactionDataTable>
+      <main className="p-3 m-3 mr-5">
+        <NavBar title="here" />
+        <TransactionDataTable></TransactionDataTable>
+        {/* Get it from axio */}
       </main>
     </>
   );
--- app/components/TransactionDataTable.jsx
+++ app/components/TransactionDataTable.jsx
@@ -1,30 +1,29 @@
-"use client"
+"use client";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 export default function TransactionDataTable() {
-
-let data= [
-  {
-    label: "home",
-    value: 10,
-    color: "rgba(0, 43, 73, 1)",
-    cutout: "50%",
-  },
-  {
-    label: "rent",
-    value:15,
-    color: "rgba(0, 103, 160, 1)",
-    cutout: "50%",
-  },
-  {
-    label: "food",
-    value: 100,
-    color: "rgba(83, 217, 217, 1)",
-    cutout: "100%",
-  },
-]
+  let data = [
+    {
+      label: "home",
+      value: 10,
+      color: "rgba(0, 43, 73, 1)",
+      cutout: "50%",
+    },
+    {
+      label: "rent",
+      value: 15,
+      color: "rgba(0, 103, 160, 1)",
+      cutout: "50%",
+    },
+    {
+      label: "food",
+      value: 100,
+      color: "rgba(83, 217, 217, 1)",
+      cutout: "100%",
+    },
+  ];
 
   const options = {
     plugins: {
@@ -46,21 +45,20 @@ let data= [
     ],
   };
 
-
-  return <>
-    <div
-        className="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]  dark:bg-neutral-700">
-        <h5
-          className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-          Card title
+  return (
+    <>
+      <div className="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]  dark:bg-neutral-700">
+        <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+          Total expense
         </h5>
         <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </p>
-        <div style={{width: '350px ', height: '350px'}}>
-        <Doughnut data={finalData} options={options}  />
+        <div style={{ width: "350px ", height: "350px" }}>
+          <Doughnut data={finalData} options={options} />
         </div>
       </div>
-  </>;
+    </>
+  );
 }
